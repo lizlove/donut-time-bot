556116c43e63694ceef2411debfbdb6c02dbc426
Alex Pear <alex@havenlife.com>
2018-03-21 15:25:20 -0400

    Revert "Actually increment the donut total"
    This reverts commit 1d59b96893f5b2d1ff3c7a3722ca0c270b476917.

--- skills/donutbot.js
+++ skills/donutbot.js
@@ -73,7 +73,7 @@ module.exports = function(controller) {
                     toSave = {
                         id: recipient.id,
                         dailyDonutsDonated: recipient.dailyDonutsDonated,
-                        lifetimeDonuts: recipient.lifetimeDonuts + count
+                        lifetimeDonuts: recipient.lifetimeDonuts
                     };
                 }
                 else {
