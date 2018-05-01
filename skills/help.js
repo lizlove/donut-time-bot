335b71d20e7cd7f1ae383298c27ca96a79106b59
liz <lizlovero@havenlife.com>
2018-05-01 12:44:16 -0400

    Update help script.

--- skills/help.js
+++ skills/help.js
@@ -1,20 +1,13 @@
 module.exports = function(controller) {
 
+    controller.hears(['help'], 'direct_message,direct_mention,mention', function (bot, message) {
+        let help = '\n Directions\nAdd a donut after someones username like this `@username : donut:`. You can include a message as well. Everyone has 6 donuts to give out per day and can only give donuts in the #donuttime channel. \n To see the donut time leaderboard, click here: https://dashboard.heroku.com/apps/donuttime';
 
-    // This before middleware allows the help command to accept sub-thread names as a parameter
-    // so users can say help to get the default thread, but help <subthread> will automatically
-    // jump to that subthread (if it exists)
-    controller.studio.before('help', function(convo, next) {
-
-        // is there a parameter on the help command?
-        // if so, change topic.
-        if (matches = convo.source_message.text.match(/^help (.*)/i)) {
-            if (convo.hasThread(matches[1])) {
-                convo.gotoThread(matches[1]);
-            }
+        if (user && user.name) {
+            bot.replyPrivate(message, `Hello ${user.name}!!!` + help);
+        } else {
+            bot.replyPrivate(message, 'Hello.');
         }
-
-        next();
 
     });
 
